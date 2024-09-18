ce150e2b85967cd47c0a39ffaa32e69bede7e660
decade <1782976211@qq.com>
2024-09-18 17:58:02 +0800

    feat: 用户注册

--- apps/user/src/user.controller.ts
+++ apps/user/src/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.userService.getHello();
+  @Post('register')
+  async register(@Body() registerUser: RegisterUserDto) {
+    return await this.userService.register(registerUser);
   }
 }
