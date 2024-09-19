289e1c0d868320a36dee829c3a6b47627dcf9eb1
decade <1782976211@qq.com>
2024-09-19 09:39:45 +0800

    refactor: 将jwtModule 放到 commonModule 引入

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -3,27 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { RedisModule } from '@app/redis';
 import { EmailModule } from '@app/email';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthGuard, CommonModule } from '@app/common';
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [
-    RedisModule,
-    EmailModule,
-    JwtModule.registerAsync({
-      global: true,
-      useFactory() {
-        return {
-          secret: 'decade',
-          signOptions: {
-            expiresIn: '30m', // 默认 30 分钟
-          },
-        };
-      },
-    }),
-    CommonModule,
-  ],
+  imports: [RedisModule, EmailModule, CommonModule],
   controllers: [UserController],
   providers: [
     UserService,
